f472489fa5fa7cbe5cbe7dda9728e81bf1948005
marcio1002 <marcioalemao1999@yahoo.com>
2022-03-26 00:30:16 -0300

    Modicação no path e no README

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ fi
 
 echo "
 # NOTIFICATION BATTERY
-  */1 * * * * DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus $PATH_CURRENT/" "battery-notify.sh" | sudo tee -a ./crontab_tmp > /dev/null
+  */1 * * * * DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus $PATH_CURRENT/""battery-notify.sh" | sudo tee -a ./crontab_tmp > /dev/null
 
 crontab -u "$USER" ./crontab_tmp
 sudo rm crontab_tmp
