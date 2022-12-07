d73efe82f1e69404c100e8f2bd9c69f7d470f8e5
marcio1002 <marcioalemao1999@yahoo.com>
2022-12-06 21:11:29 -0300

    Modifications to installation variables and configuration

--- battery-notify.sh
+++ battery-notify.sh
@@ -1,41 +1,45 @@
-#!/bin/bash
+#!/bin/bash -l
 #   - - - - - - - - - - - - - -
 # |                            |
 # |  Verify status of battery  |
 # |                            |
-#  - - - - - - - - - - - - - - 
+#  - - - - - - - - - - - - - -
 
-LANGUAGE=$(locale | grep -Eo '^LANGUAGE=\w{5}' | cut -d '=' -f 2)
-PERCENT=$(cat /sys/class/power_supply/BAT0/capacity)
-STATUS=$(cat /sys/class/power_supply/BAT0/status)
-
-PERCENT_CRITICAL_DEFAULT=38
+PERCENT_CRITICAL_DEFAULT=30
 PERCENT_FULL_DEFAULT=100
+NOTIFY_PATH_LOG="/tmp/battery_notify.conf"
 
-NOTIFICATION=$(cat /tmp/battery_notify.conf || echo 'listen')
+NOTIFY_LANGUAGE=$(locale | grep -Eo '^LANGUAGE=\w{5}' | cut -d '=' -f 2)
+NOTIFY_PERCENT=$(cat /sys/class/power_supply/BAT0/capacity)
+NOTIFY_STATUS=$(cat /sys/class/power_supply/BAT0/status)
 
-if [ "$PERCENT" -gt "${NOTIFY_PERCENT_CRITICAL:-$PERCENT_CRITICAL_DEFAULT}" ] && [ "$PERCENT" -lt "${NOTIFY_PERCENT_LOW:-$PERCENT_FULL_DEFAULT}" ]; then
+NOTIFICATION=$(cat $NOTIFY_PATH_LOG || echo 'listen')
 
-    echo "listen" > /tmp/battery_notify.conf
+if [ "$NOTIFY_PERCENT" -gt "${NOTIFY_PERCENT_CRITICAL:-$PERCENT_CRITICAL_DEFAULT}" ] &&
+    [ "$NOTIFY_PERCENT" -lt "${NOTIFY_PERCENT_LOW:-$PERCENT_FULL_DEFAULT}" ]; then
+
+    echo "listen" >$NOTIFY_PATH_LOG
 fi
 
-if [ "$PERCENT" -le "${NOTIFY_PERCENT_CRITICAL:-$PERCENT_CRITICAL_DEFAULT}" ] && [ "$STATUS" == "Discharging" ] && [ "$NOTIFICATION" == "listen" ]; then
-    if [ "$LANGUAGE" == "pt_BR" ]; then
-        notify-send -u critical -t 50000 -i battery-caution "Bateria em $PERCENT%" "É recomendado recarregar a bateria"
+if [ "$NOTIFY_PERCENT" -le "${NOTIFY_PERCENT_CRITICAL:-$PERCENT_CRITICAL_DEFAULT}" ] &&
+    [ "$NOTIFY_STATUS" = "Discharging" ] && [ "$NOTIFICATION" = "listen" ]; then
+    if [ "$NOTIFY_LANGUAGE" == "pt_BR" ]; then
+        notify-send -u critical -t 50000 -i battery-caution "Bateria em $NOTIFY_PERCENT%" "É recomendado recarregar a bateria"
     else
-        notify-send -u critical -t 50000 -i battery-caution "$PERCENT% battery" "It is recommended to recharge the battery"
+        notify-send -u critical -t 50000 -i battery-caution "$NOTIFY_PERCENT% battery" "It is recommended to recharge the battery"
     fi
 
-    echo "not-listen" > /tmp/battery_notify.conf
+    echo "not-listen" >$NOTIFY_PATH_LOG
 fi
 
-if [ "$PERCENT" -eq "${NOTIFY_PERCENT_LOW:-$PERCENT_FULL_DEFAULT}" ] &&  [ "$STATUS" == "Full" ] && [ "$NOTIFICATION" == "listen" ]; then
+if [ "$NOTIFY_PERCENT" -eq "${NOTIFY_PERCENT_LOW:-$PERCENT_FULL_DEFAULT}" ] &&
+    [ "$NOTIFY_STATUS" = "Full" ] && [ "$NOTIFICATION" = "listen" ]; then
 
-    if [ "$LANGUAGE" == "pt_BR" ]; then
-        notify-send -u normal -t 50000 -i  battery-full 'Bateria cheia'
+    if [ "$NOTIFY_LANGUAGE" == "pt_BR" ]; then
+        notify-send -u low -t 50000 -i battery-full 'Bateria cheia'
     else
-        notify-send -u normal -t 50000 -i battery-full 'Battery full'
+        notify-send -u low -t 50000 -i battery-full 'Battery full'
     fi
 
-    echo "not-listen" > /tmp/battery_notify.conf
+    echo "not-listen" >$NOTIFY_PATH_LOG
 fi
--- install.sh
+++ install.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 USER=$(whoami)
-PATH_CURRENT=$(pwd)
 CRONTAB=$(command -v crontab)
 
 if [ -z "$CRONTAB" ]; then
-    echo -e "\e[00;31;1mcrontab not found\e[m"
-    exit 1
+  echo -e "\e[00;31;1mcrontab not found\e[m"
+  exit 1
 fi
 
-( $CRONTAB -u "$USER" -l || echo '' ) | sudo tee ./crontab_tmp > /dev/null
+($CRONTAB -u "$USER" -l || echo '') | sudo tee ./crontab_tmp >/dev/null
 
 echo "
 # NOTIFICATION BATTERY
-  */1 * * * * DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus $PATH_CURRENT/""battery-notify.sh" | sudo tee -a ./crontab_tmp > /dev/null
+NOTIFY_PERCENT_CRITICAL=30
+NOTIFY_PERCENT_LOW=90
+
+*/1 * * * * DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus /home/marcio/battery-notify/battery-notify.sh" |
+  sudo tee -a ./crontab_tmp >/dev/null
 
 crontab -u "$USER" ./crontab_tmp
 sudo rm crontab_tmp
